2233a7a6a053b72275c4768d36e41fc8f0ef5f17
Muhammad Rizal M <mrizal.muhaimin.staff@dansmultipro.com>
2023-06-24 23:38:54 +0700

    add ui

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,11 +1,10 @@
+import { Onboarding } from "../home/Onboarding.jsx";
 
 export const Home = () => {
   
-
   return (
     <div className="w-full bg-white h-screen">
-      <p className="font-[alice] text-steel700">AAAAAAAAAA AAAAAAAAAAAAA aaaaaaaaaa aaaaaaaaaaa aaaa</p>
-      <p className="font-[tanPearl] text-steel700">akram</p>
+      <Onboarding/>
     </div>
   );
 };
