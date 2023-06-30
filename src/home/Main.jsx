e017a95b84039ba71605eafa53c57c586be0e24a
Faziz <faris.aziz@bfi.co.id>
2023-06-30 23:51:38 +0700

    remove commented code

--- src/home/Main.jsx
+++ src/home/Main.jsx
@@ -3,21 +3,18 @@ import { MainBottomSection } from "./MainBottomSection";
 import { BottomBar } from "../components/BottomBar";
 
 export const Main = ({dataInvitation={}}) => {
-  // const refCalendarSection = useRef(null);
   const refQRSection = useRef(null);
   const refGiftSection = useRef(null);
 
-  // const [heightCalendarSection, setHeightCalendarSection] = useState(0);
   const [heightQRSection, setHeightQRSection] = useState(0);
   const [heightGiftSection, setHeightGiftSection] = useState(0);
 
   const [activeSection, setActiveSection] = useState('CALENDAR');
 
   useEffect(() => {
-    // setHeightCalendarSection(refCalendarSection.current.getBoundingClientRect().top);
     setHeightQRSection(refQRSection.current.getBoundingClientRect().top + 55);
     setHeightGiftSection(refGiftSection.current.getBoundingClientRect().top + 55);
-  }, [/*refCalendarSection.current , */refQRSection.current, refGiftSection.current]);
+  }, [refQRSection.current, refGiftSection.current]);
 
   const scrollEvent = (e) => { 
     const currentHeight = e.target.scrollTop + e.target.offsetHeight;
