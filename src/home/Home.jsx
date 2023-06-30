fc12aaa78b632477d132f1cd81e895a48f10a1ab
Muhammad Rizal M <mrizal.muhaimin.staff@dansmultipro.com>
2023-06-30 14:58:09 +0700

    setup axios

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,10 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { Onboarding } from "../home/Onboarding.jsx";
 import { Quiz } from "../home/Quiz.jsx";
 import { Main } from './Main.jsx';
 
+import { getInvitation } from "./../services/invitations"
+
 export const Home = () => {
+  const { id: idInvitation } = useParams();
   const [page, setPage] = useState('onbaordingPage');
+  const [dataInvitation, setDataInvitation] = useState([]);
+
+  const getDataInvitation = async () => {
+    console.log(idInvitation);
+    const response = await getInvitation({
+      invitationId:idInvitation
+
+    });
+    console.log(response);
+    const data = response?.data?.response?.data || {};
+    console.log(data);
+    setDataInvitation(data);
+    console.log(dataInvitation);
+
+  }
+
+  useEffect(() => {
+    getDataInvitation()
+    
+  }, []);
 
   function renderContent (){
     switch (page) {
